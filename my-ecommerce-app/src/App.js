d96428688a94f8779259cc39f12e6fcbf908762c
Mariem <Mariemouertatani2001@gmail.com>
2024-04-03 00:27:15 -0600

    added product files

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,16 +1,21 @@
-// Example using BrowserRouter
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import Homepage from './components/Homepage';
+import Productpage from './components/Productpage';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="App">
-        <LoginPage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<Productpage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
